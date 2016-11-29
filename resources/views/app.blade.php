7c5c9c3b46c4636257679c90a0c0f881acb4a6c8
Manikandan Swaminathan <mani.bk31@gmail.com>
2016-11-29 16:21:03 +0000

    initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Diet App</title>
+        <link rel="stylesheet" type="text/css" href="{{asset('semantic/dist/semantic.min.css')}}">
+        <script src="{{asset('dist/semantic.min.js')}}"></script>
+        <script
+          src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+          integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc="
+          crossorigin="anonymous"></script>
+        
+    </head>
+    <body>
+        Working!!
+    </body>
+</html>
